2173b1481ecd624b4dce5fe1d2a181cf3d346f5e
Minhhau32223 <hau344779@gmail.com>
2024-05-15 17:26:42 -0700

    phanquyenganxong

--- doAn/admin/page/AHeader.php
+++ doAn/admin/page/AHeader.php
@@ -1,3 +1,26 @@
+<?php
+require_once('../../db_connect.php');
+require_once('../../role_check.php');
+$conn = new Database();
+$features_list = $conn->query("SELECT * FROM chucnang");
+
+$userAuth = new userAuth($conn);
+$view_permission_list = $userAuth->view_permission_list;
+
+$conn->close();
+
+function checkReadPermission($view_permission_list, $feature_id)
+{
+  if (in_array($feature_id, $view_permission_list)) {
+    return ' ';
+  } else {
+    return 'hidden';
+  }
+}
+
+
+?>
+
 <html>
 
 <head>
@@ -20,19 +43,19 @@
                 </div>
 
                 <div class="menu">
-                    <div class="option" id="thongke"><a href="AHome.php?chon=t&id=thongke" onclick="selectOption(this)">Thống kê</a></div>
-                    <div class="option" id="sanpham"><a href="AHome.php?chon=t&id=sanpham" onclick="selectOption(this)">Sản phẩm</a></div>
-                    <div class="option" id="phieunhap"><a href="AHome.php?chon=t&id=phieunhap">Phiếu nhập</a></div>
-                    <div class="option" id="donhang"><a href="AHome.php?chon=t&id=donhang">Đơn hàng</a></div>
-                    <div class="option" id="nhacungcap"><a href="AHome.php?chon=t&id=nhacungcap">Nhà cung cấp</a></div>
-                    <div class="option" id="nguoidung"><a href="AHome.php?chon=t&id=nguoidung">Người dùng</a></div>
+                    <div class="option <?=checkReadPermission($view_permission_list, "CN009")?>" id="thongke"><a href="AHome.php?chon=t&id=thongke" onclick="selectOption(this)">Thống kê</a></div>
+                    <div class="option <?=checkReadPermission($view_permission_list, "CN003")?>" id="sanpham"><a href="AHome.php?chon=t&id=sanpham" onclick="selectOption(this)">Sản phẩm</a></div>
+                    <div class="option <?=checkReadPermission($view_permission_list, "CN007")?>" id="phieunhap"><a href="AHome.php?chon=t&id=phieunhap">Phiếu nhập</a></div>
+                    <div class="option <?=checkReadPermission($view_permission_list, "CN005")?>"  id="donhang"><a href="AHome.php?chon=t&id=donhang">Đơn hàng</a></div>
+                    <div class="option <?=checkReadPermission($view_permission_list, "CN008")?>" id="nhacungcap"><a href="AHome.php?chon=t&id=nhacungcap">Nhà cung cấp</a></div>
+                    <div class="option <?=checkReadPermission($view_permission_list, "CN004")?>" id="nguoidung"><a href="AHome.php?chon=t&id=nguoidung">Người dùng</a></div>
                     <div class="option" id="cart">Khác
                         <div class="sub-menu">
                             <ul>
-                                <li><a href="AHome.php?chon=t&id=quyen">Quyền</a></li>
-                                <li><a href="AHome.php?chon=t&id=khuyenmai">Khuyến mãi</a></li>
-                                <li><a href="AHome.php?chon=t&id=vanchuyen">Vận chuyển</a></li>
-                                <li><a href="AHome.php?chon=t&id=thuonghieu">Thương hiệu</a></li>
+                                <li class="<?=checkReadPermission($view_permission_list, "CN006")?>"><a href="AHome.php?chon=t&id=quyen">Quyền</a></li>
+                                <li class="<?=checkReadPermission($view_permission_list, "CN010")?>"><a href="AHome.php?chon=t&id=khuyenmai">Khuyến mãi</a></li>
+                                <li class="<?=checkReadPermission($view_permission_list, "CN011")?>"><a href="AHome.php?chon=t&id=vanchuyen">Vận chuyển</a></li>
+                                <li class="<?=checkReadPermission($view_permission_list, "CN012")?>"><a href="AHome.php?chon=t&id=thuonghieu">Thương hiệu</a></li>
                             </ul>
                         </div>
                     </div>
--- doAn/admin/page/insert_new_pn.php
+++ doAn/admin/page/insert_new_pn.php
@@ -16,12 +16,23 @@ if(isset($_SESSION['item_add']))
         foreach($data as $item){
             $Masp=$item['Masp'];
             $Soluong=$item['Soluong'];
-          
+
             $sql="INSERT INTO chitietphieunhap(maPhieunhap,Masp,soluong) VALUES('$Mapn','$Masp','$Soluong')";
             $result_ctpn=mysqli_query($conn,$sql);
             if($result_ctpn)
-            {
+            {   
+                $sql_check_product = mysqli_query($conn, "SELECT * FROM sanpham WHERE Masp = '$Masp'");
+        $num_rows = mysqli_num_rows($sql_check_product);
+
+        if ($num_rows > 0) {
+            // Sản phẩm đã tồn tại, tăng số lượng sản phẩm
+            $row = mysqli_fetch_assoc($sql_check_product);
+            $soluongconlai = $row['Soluongconlai'] + $Soluong;
+
+            $sql_update_product = mysqli_query($conn, "UPDATE sanpham SET Soluongconlai = '$soluongconlai' WHERE Masp = '$Masp'");
+        }
                 echo "Thêm thành công";
+
             }
             else
             {
